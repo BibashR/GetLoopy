4c037967eb410ba2b7eeb6859c777c1a88d97ab9
brwak <wakchhalibr@gmail.com>
2025-03-03 20:45:39 -0500

    Finished PartA

---
+++ src/PartA.java
@@ -0,0 +1,33 @@
+public class PartA {
+    public static void main(String[] args) {
+        int cnt;
+
+        // Task 1
+        System.out.print("Task1: ");
+        for (cnt= 0; cnt <= 30; cnt++) {
+            System.out.print(cnt + " ");
+        }
+        System.out.println(" ");
+
+        // Task  2
+        System.out.print("Task2: ");
+        for (cnt= 30; cnt >= 0; cnt--) {
+            System.out.print(cnt + " ");
+        }
+        System.out.println("");
+
+        // Task  3
+        System.out.print("Task3: ");
+        for (cnt= 0; cnt <= 18; cnt+=3) {
+            System.out.print(cnt + " ");
+        }
+        System.out.println("");
+
+        // Task  4
+        System.out.print("Task4: ");
+        for (cnt= 10; cnt >= 0; cnt-=2) {
+            System.out.print(cnt + " ");
+        }
+        System.out.println("");
+        }
+    }
